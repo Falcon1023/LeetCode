c49d03ac997a2e9f46a0fafbb8727100936d2a01
trierbo <trier@keessi.com>
2017-08-16 16:56:07 +0800

    leetcode 9 10

---
+++ java/me/trierbo/LeetCode/PalindromeNumber_9/Solution.java
@@ -0,0 +1,14 @@
+package me.trierbo.LeetCode.PalindromeNumber_9;
+
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) return false;
+        int tmp = x, res = 0;
+        while (x != 0) {
+            if (res > Integer.MAX_VALUE / 10) return false;
+            res = res * 10 + x % 10;
+            x /= 10;
+        }
+        return tmp == res;
+    }
+}
