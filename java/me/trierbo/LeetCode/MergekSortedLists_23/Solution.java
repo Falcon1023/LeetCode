8dfd074e8482c916689ff8081691e5eec3beff51
trierbo <trier@keessi.com>
2017-09-08 21:12:00 +0800

    Merge k Sorted Lists

---
+++ java/me/trierbo/LeetCode/MergekSortedLists_23/Solution.java
@@ -0,0 +1,37 @@
+package me.trierbo.LeetCode.MergekSortedLists_23;
+
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) return null;
+        PriorityQueue<ListNode> queue = new PriorityQueue<>(lists.length, new Comparator<ListNode>() {
+            @Override
+            public int compare(ListNode o1, ListNode o2) {
+               return Integer.compare(o1.val, o2.val);
+            }
+        });
+
+        for (ListNode listNode : lists) {
+            if (listNode != null)
+                queue.add(listNode);
+        }
+
+        ListNode head = new ListNode(0);
+        ListNode tail = head;
+        while (!queue.isEmpty()) {
+            tail.next = queue.poll();
+            tail = tail.next;
+            if (tail.next != null)
+                queue.add(tail.next);
+        }
+        return head.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
