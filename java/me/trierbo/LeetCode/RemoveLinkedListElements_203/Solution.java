770ca62a86b81dfaf47a9349b6be5ad52515baef
trierbo <trier@keessi.com>
2017-09-09 13:15:52 +0800

    Remove LinkedList Elements

---
+++ java/me/trierbo/LeetCode/RemoveLinkedListElements_203/Solution.java
@@ -0,0 +1,24 @@
+package me.trierbo.LeetCode.RemoveLinkedListElements_203;
+
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode node = new ListNode(0);
+        ListNode res = node;
+        while (head != null) {
+            if (head.val != val) {
+                node.next = head;
+                node = node.next;
+            }
+            head = head.next;
+            if (head == null)
+                node.next = null;
+        }
+        return res.next;
+    }
+}
+
+ class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+ }
