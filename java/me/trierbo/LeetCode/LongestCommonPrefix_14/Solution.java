efcbaaaeb156443b473fc81f035c1738b192279e
trierbo <trier@keessi.com>
2017-09-10 21:15:11 +0800

    Longest Common Prefix

---
+++ java/me/trierbo/LeetCode/LongestCommonPrefix_14/Solution.java
@@ -0,0 +1,21 @@
+package me.trierbo.LeetCode.LongestCommonPrefix_14;
+
+import java.util.Arrays;
+
+public class Solution {
+    public String longestCommonPrefix(String[] strs) {
+       StringBuilder res = new StringBuilder();
+       if (strs != null && strs.length != 0) {
+           Arrays.sort(strs);
+           char[] first = strs[0].toCharArray();
+           char[] last = strs[strs.length - 1].toCharArray();
+           for (int i = 0; i < first.length; i++) {
+               if (last.length > i && last[i] == first[i])
+                   res.append(first[i]);
+               else
+                   break;
+           }
+       }
+       return res.toString();
+    }
+}
