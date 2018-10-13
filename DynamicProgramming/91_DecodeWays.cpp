7143034300dc0cadea402c519cc69e6216affe1e
trierbo <baochuanyan@gmail.com>
2018-10-13 15:15:17 +0800

    fix 91. Decode Ways

--- DynamicProgramming/91_DecodeWays.cpp
+++ DynamicProgramming/91_DecodeWays.cpp
@@ -6,21 +6,26 @@ public:
     int numDecodings(string s) {
         if (s.empty())
             return 0;
-        return subDecodings(s, s.begin(), s.end());
+        int flag[s.size()];
+        for (int i = 0; i < s.size(); i++)
+            flag[i] = -1;
+        return subDecodings(s, flag, 0, s.size());
     }
 
-    int subDecodings(string &s, string::iterator beg, string::iterator end) {
-        if (*beg == '0')
+    int subDecodings(string &s, int flag[], int beg, int end) {
+        if (s[beg] == '0')
             return 0;
         if (beg == end || (beg+1) == end)
             return 1;
 
-        int sub1 = 0, sub2 = 0;
-        if (*(beg + 1) != '0')
-            sub1 = subDecodings(s, beg+1, end);
-        if (((*beg - '0') * 10 + (*(beg + 1) - '0')) <= 26)
-            sub2 = subDecodings(s, beg+2, end);
-
-        return sub1 + sub2;
+        if (flag[beg] == -1) {
+            int sub1 = 0, sub2 = 0;
+            if (s[beg + 1] != '0')
+                sub1 = subDecodings(s, flag, beg+1, end);
+            if (((s[beg] - '0') * 10 + (s[beg + 1] - '0')) <= 26)
+                sub2 = subDecodings(s, flag, beg+2, end);
+            flag[beg] = sub1 + sub2;
+        }
+        return flag[beg];
     }
 };
