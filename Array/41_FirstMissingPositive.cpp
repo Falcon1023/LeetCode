551bc2c87afd7fb9c325439450ce0c0c8189dfee
trierbo <baochuanyan@gmail.com>
2018-11-06 23:11:58 +0800

    fix 41. First Missing Positive

--- Array/41_FirstMissingPositive.cpp
+++ Array/41_FirstMissingPositive.cpp
@@ -7,7 +7,7 @@ public:
         auto size = nums.size();
         for (decltype(size) i = 0; i < size; ++i) {
             int temp = nums[i];
-            while (temp > 0 && temp <= size && temp != i + 1 && temp != nums[temp-1]) {       
+            while (temp > 0 && temp <= size && temp != i + 1 && temp != nums[temp-1]) {
                 nums[i] = nums[temp-1];
                 nums[temp-1] = temp;
                 temp = nums[i];
